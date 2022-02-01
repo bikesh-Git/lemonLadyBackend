481ff3fd151bf62fdc2e08b806a3cbb317db5558
unknown <bikeshbitbucket>
2022-02-01 13:19:11 +0530

    make it better

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.use('/api/checkout',userStripe)
 
 
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+// });
 
 
 app.listen(process.env.PORT || 5000 ,()=>{
